223987a6551367f2c2121e2800b34f475cc9ddb2
Ismael Barbosa <ibf321@gmail.com>
2017-10-11 22:52:19 -0300

    alteração de db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create name: 'Ismael Barbosa', username: 'ismael'
+User.create name: 'Cicero Akiles', username: 'akiles'
+User.create name: 'Ramon Felizardo', username: 'ramon'
+Loja.create name: 'Farmácia Anamaria', descrcao: 'Sua Farmácia: tudo pra você e sua familia', tag: 'farmacia', segmente: 'saude'
+Loja.create name: 'Farmácia Gabriela', descrcao: 'Sua Farmácia: tudo pra você e sua familia', tag: 'farmacia', segmente: 'saude'
+Loja.create name: 'Academina FAFA', descrcao: 'Sua Academia: vida fit', tag: 'academina', segmente: 'saude'
+Loja.create name: 'Farmácia Anamaria', descrcao: 'Sua Academia: vida fit', tag: 'academia', segmente: 'saude'
