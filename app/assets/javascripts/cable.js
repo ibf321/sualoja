ba1ad43e2a78cdfdb4ed40260664ef696afc2b37
Ismael Barbosa <ibf321@gmail.com>
2017-10-16 21:04:11 -0300

    16102017

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -14,3 +14,9 @@
      $('.modal').modal();
    });
 }).call(this);
+
+$(document).ready(function(){
+  // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+  $('.modal').modal();
+});
+        
